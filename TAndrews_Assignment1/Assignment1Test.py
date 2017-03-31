34d3751237252251066ad470f4cae530c21e41aa
Tyler Andrews <andre205@mail.chapman.edu>
2017-03-31 15:24:06 -0700

    Delete Assignment1Test.py

--- TAndrews_Assignment1/Assignment1Test.py
+++
@@ -1,8 +0,0 @@
-pretaxprice=input("How much does the item cost?")
-
-taxrate=input("What is the sales tax, given as a percent?")
-
-taxdecimal=(taxrate)/100
-posttaxprice=(pretaxprice)*(1 + taxdecimal)
-
-print("After tax, your item will cost,",posttaxprice)
